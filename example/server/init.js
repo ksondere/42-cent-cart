6af555e7ce27d90549b48c98e9f286633ba2c7f0
Kelly Sonderegger <ksondere@gmail.com>
2015-05-29 09:49:59 -0600

    sufficient for initial package release

--- example/server/init.js
+++ example/server/init.js
@@ -1,9 +1,30 @@
-if(Products.find({}).count() === 0){
-	console.log('load sample products');
-	for(i = 0; i < 6; i++){
-		Products.insert({
-			name: Random.choice(['Cool', 'Sweet', 'Wowzer', 'Nice']) + " Shirt",
-			price: Math.floor(Math.random()*10000)/100
-		});
-	}
+if (Products.find({}).count() === 0) {
+    console.log('load sample products');
+
+    Products.insert({
+        name: 'Apple Tree',
+        price: 49.67,
+        image: 'AppleTree.jpg'
+    });
+    Products.insert({
+        name: 'Birch Tree',
+        price: 99.67,
+        image: 'BirchTree1.jpg'
+    });
+    Products.insert({
+        name: 'Cherry Tree',
+        price: 62.67,
+        image: 'AppleTree.jpg'
+    });
+    Products.insert({
+        name: 'Maple Tree',
+        price: 49.67,
+        image: 'MapleTreeGreen.jpg'
+    });
+    Products.insert({
+        name: 'Olive Tree',
+        price: 349.67,
+        image: 'OliveTree.jpg'
+    });
+
 }
